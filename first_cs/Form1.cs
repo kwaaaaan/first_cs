8bf3cfc2180665f2dad1a432e05f6c7562e7731c
KWAN <kjg9406@gmail.com>
2023-11-06 16:57:39 +0900

    Add project files.

---
+++ first_cs/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //MessageBox.Show("안녕하세요!");
+
+            textBox_print.Text = "나는\r\n피곤합니다.\r\n";
+            //windows환경에서 줄바꿈은 \r\n임! c++과 c의 콘솔창은 DOS환경이라서 \n로 가능(옛날 mac은 \r임)
+        }
+    }
+}
